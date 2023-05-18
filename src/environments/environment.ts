5a3f6637a7740a06901dc8052fa919833c437fbb
yaseminb13 <76526383+yaseminb13@users.noreply.github.com>
2023-05-18 16:05:54 +0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyC2LsDjJlBJ1hSDnXZCMvhlEfkUWANPeL4',
+    authDomain: 'ionicfirebase-efc09.firebaseapp.com',
+    projectId: 'ionicfirebase-efc09',
+    storageBucket: 'ionicfirebase-efc09.appspot.com',
+    messagingSenderId: '473800955612',
+    appId: '1:473800955612:web:afd40ac49d280732478619'
+  }
+};
